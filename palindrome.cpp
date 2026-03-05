e6c2a09a098bce04a11e040982c6d953508532f7
vedantraicr7 <vedantraicr7@gmail.com>
2026-03-05 18:36:13 +0530

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int num, original, reversed = 0, remainder;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    original = num;
+
+    while (num != 0) {
+        remainder = num % 10;          // get last digit
+        reversed = reversed * 10 + remainder; 
+        num = num / 10;                // remove last digit
+    }
+
+    if (original == reversed)
+        cout << "Palindrome Number";
+    else
+        cout << "Not a Palindrome Number";
+
+    return 0;
+}
